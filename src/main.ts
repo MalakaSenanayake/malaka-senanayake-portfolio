743d5d2a185dd734f58fd8f87842d1be96fad115
dilansalinda <contact.dilansalinda@gmail.com>
2025-09-26 19:42:59 +0530

    issue fixed and enabled pwa

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { appProviders } from './app/app.config';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(HttpClientModule, BrowserAnimationsModule),
+    importProvidersFrom(BrowserAnimationsModule),
     ...appProviders,
-    provideRouter(routes)
+    provideRouter(routes),provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch(err => console.error(err));
